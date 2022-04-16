d9fd3568e1b92d26cdaebf284d289b3dd14392a1
Luke C <lpcolias@gmail.com>
2022-04-15 19:47:26 -0700

    Update App.js
    More Script PRactice

--- my-website/src/App.js
+++ my-website/src/App.js
@@ -1,32 +1,60 @@
 import logo from './logo.svg';
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 
 
-function Checkbox() {
-  const [checked, setChecked] = React.useState(false);
-  React.useEffect(() => {
-    alert(`checked: ${checked.toString()}`);
-  })
+function GitHubUser({login}) {
+  const [data, setData] = React.useState(null);
+  
+  useEffect(() => {
+    fetch(`https://api.github.com/users/${login}`)
+      .then(res => res.json())
+      .then(setData)
+      .catch(console.error);
+  }, []);
 
-  return (
+
+  if(data) {
+    return(
+      <div>
+        <h1>{data.login}</h1>
+        <img src={data.avatar_url} />
+
+      </div>
+    );
+  }
+
+  return null;
+}
+
+
+function Checkbox() {
+
+  const [checked, toggle] = React.useReducer(
+    checked => !checked, 
+    false
+  );
+
+
+  return(
     <>
       <input 
-      type="checkbox" 
-      value={checked} 
-      onChange={() => 
-          setChecked(checked => !checked)
-        }
-      />
+        type="checkbox" 
+        value={checked} 
+        onChange={toggle}
+        />
       {checked ? "checked" : "not checked"}
     </>
-  )
+  );
+
 }
 
 function App(props) {
+
   return (
-   <Checkbox />
-  );
+    //<GitHubUser login="lcolias"/>
+    <Checkbox />
+  )
   
 }
 
