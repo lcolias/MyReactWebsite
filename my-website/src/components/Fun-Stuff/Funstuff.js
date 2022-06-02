1e05437b8cbbe236729d3b12cb42d0b2c238481d
Luke C <lpcolias@gmail.com>
2022-06-01 19:57:26 -0700

    Updates to Navbar and React Practice
    Updated the Navigation Bar to render with a mobile-first approach. The base styles create a dropdown style menu that contains all of the page options.
    
    The React Practice is done in parallel with the linkedIn learning "React.js: Building an interface" course.

--- my-website/src/components/Fun-Stuff/Funstuff.js
+++ my-website/src/components/Fun-Stuff/Funstuff.js
@@ -2,10 +2,13 @@ import React from "react";
 
 import "./Funstuff.css";
 
+import Search from "../Search/Search";
+
 function Funstuff() {
     return(
         <>
             <h1>Fun-Stuff Page!</h1>
+            <Search />
         </>
     );
 }
