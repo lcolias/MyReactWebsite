e33795081a92e283c333951ff7abdeeb12d7da05
Luke C <lpcolias@gmail.com>
2022-12-10 19:16:23 -0800

    Update to "About" Page
    Updated personal Biography paragraph.
    Updated Personal/Educational Experience Paragraph.

--- my-website/src/components/About/About.js
+++ my-website/src/components/About/About.js
@@ -44,7 +44,7 @@ function GitHubUser({login}) {
               </div>
 
               <div className="main-info data" >
-                <div className="spacing">{data.name}</div>
+                <div className="spacing"><em>{data.name}</em></div>
                 <div className="spacing">{data.location}</div>
                 <div className="spacing">{data.company}</div>
                 <div className="spacing">{data.bio}</div>
@@ -75,21 +75,30 @@ function About({login}) {
 
           <GitHubUser login={login}/>
 
-          <div class="section-content-wrapper">
-            <h1>Lucas Colias</h1>
-            <h2>Software Engineer and Spirited Geek</h2>
-                    
-            <h1>I've never been one for comprehensively cocky characterizations of myself... </h1>
-            <h3>... but a little bit of flashiness never hurt nobody  </h3>
-
-
-            <p>I graduated as a Computer Science major from California State University, Long Beach. At University, I gained a thorough understanding of programming data structures and algorithms in Java and C++. In addition, I have a solid foundation in AI and Android mobile application development both from University and personal projects. As a software engineering intern I had hands-on experience developing client and server code as well as full-stack web applications. I am adaptable, excited, and motivated when working with new technologies. I love stepping outside of my comfort zone to produce work that I am proud of.</p>
-            </div>
-          </header>
+          <div className="section-content-wrapper content-spacing">
+            
+            <p>
+              Hello World, my name is <em>Lucas</em>, but I usually go by Luke. I am currently working as a Software Engineer at Northrup Grumman. 
+              My role there involves designing and implementing updates for the F-35 Joint Stike Fighter (JSF). My favorite moments when working 
+              on any project happen when I’m able to develop Simple and Effective code. I also understand that sometimes things are complicated 
+              and those are the times I use all of resources afforded to me to figure out a solution. I’m a simple man who appreciates simple things, 
+              and I try to maintain that mindset with everything I do. Please checkout my “Contact” page for info on how to get in touch!
+            </p>
+            
+            <p>
+              I graduated as a Computer Science major from California State University, Long Beach. At University, I gained a thorough understanding 
+              of programming data structures and algorithms in Java and C++. In addition, I have a solid foundation in AI and Android mobile application 
+              development both from University and personal projects. As a software engineering intern I had hands-on experience developing client and 
+              server code as well as full-stack web applications. I am adaptable, excited, and motivated when working with new technologies. I love 
+              stepping outside of my comfort zone to produce work that I am proud of.
+            </p>
+            
+          </div>
+        </header>
 
         {/*<!-- ******************  EDUCATION & CERTIFICATIONS ****************** -->*/}
-        <section class="education">
-          <div class="section-content-wrapper">
+        <section className="education">
+          <div className="section-content-wrapper">
             <h2>Education</h2>
 
             {/*<!-- Copy this whole <section> block to add more schools. -->*/}
