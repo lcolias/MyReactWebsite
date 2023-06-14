0a7005960fd7882c874e5429e2aef3e84367e36a
Luke C <lpcolias@gmail.com>
2023-06-13 23:47:18 -0700

    Contact Page update
    Check TODO.md
    ---^ 6/13/2023 ^---

--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
+import reportWebVitals from "./reportWebVitals";
 import {BrowserRouter as Router} from "react-router-dom";
 import ScrollToTop from './effects/ScrollToTop/ScrollToTop';
 
-
-ReactDOM.render(
-  //<React.StrictMode>
+const root = createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
     <Router>
       <ScrollToTop />
       <App authorized={true}/>
     </Router>,
-  //</React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
+reportWebVitals();
 
