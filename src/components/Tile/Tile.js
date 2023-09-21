5151e9f451500156ee05d13dcb1cbfe3f8e23eda
Luke C <lpcolias@gmail.com>
2023-09-20 18:31:24 -0700

    Tile Component Updates Pt.1
    -Tiles resized
    -imgs reflect technologies used in project.
    -Tech icon for React website moved to public/images/icons folder

--- src/components/Tile/Tile.js
+++ src/components/Tile/Tile.js
@@ -11,8 +11,10 @@ function Tile(props){
     return(
         <>
             <div className={`tile-container ${props.className}`}>
-                <img src={projectImagePlaceholder} alt='Project Photo' />
                 <h1>{props.repo ? props.repo.name : 'Project Template'}</h1>
+                <div className='tech-img-list'>
+                    <img src={projectImagePlaceholder} alt='Project Photo' />
+                </div>
                 <p>{props.repo ? props.repo.description : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud...'}  </p>
                 <a href={props.repo ? props.repo.html_url : ''} target="_blank" rel="noopener noreferrer">more</a>
             </div>
